27d0b69831479100e8e8940d3047026964048de0
Jonathan YUE CHUN <jonathan.yue-chun@mythalesgroup.io>
2020-05-07 14:12:58 +0200

    added python udf example

---
+++ spark_pyspark_udf/udf_python_starter_kit/udf_example/udf0_example.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# License Agreement
+# This code is licensed under the outer restricted Tiss license:
+#
+#  Copyright [2014]-[2019] Thales Services under the Thales Inner Source Software License
+#  (Version 1.0, InnerPublic -OuterRestricted the "License");
+#
+#  You may not use this file except in compliance with the License.
+#
+#  The complete license agreement can be requested at contact@punchplatform.com.
+#
+#  Refer to the License for the specific language governing permissions and limitations
+#  under the License.
+
+import random
+
+
+def test_random() -> int:
+    return random
+        .randint(0, 100)
