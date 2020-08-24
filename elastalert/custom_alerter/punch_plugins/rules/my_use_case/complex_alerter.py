31cda3115ccfb90b985ef58a15296a0a0c1261ba
Jonathan YUE CHUN <jonathan.yue-chun@mythalesgroup.io>
2020-08-24 17:09:36 +0200

    added elastalert custom rule starter

---
+++ elastalert/custom_alerter/punch_plugins/rules/my_use_case/complex_alerter.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import dateutil.parser
+
+from elastalert.ruletypes import RuleType
+
+# elastalert.util includes useful utility functions
+# such as converting from timestamp to datetime obj
+from elastalert.util import ts_to_dt  # NOQA
+
+
+class ComplexAlerter(RuleType):
+
+    # By setting required_options to a set of strings
+    # You can ensure that the rule config file specifies all
+    # of the options. Otherwise, ElastAlert will throw an exception
+    # when trying to load the rule.
+    required_options = set(["time_start", "time_end", "usernames"])
+
+    # add_data will be called each time Elasticsearch is queried.
+    # data is a list of documents from Elasticsearch, sorted by timestamp,
+    # including all the fields that the config specifies with "include"
+    def add_data(self, data):
+        for document in data:
+
+            # To access config options, use self.rules
+            if document["username"] in self.rules["usernames"]:
+
+                # Convert the timestamp to a time object
+                login_time = document["@timestamp"].time()
+
+                # Convert time_start and time_end to time objects
+                time_start = dateutil.parser.parse(self.rules["time_start"]).time()
+                time_end = dateutil.parser.parse(self.rules["time_end"]).time()
+
+                # If the time falls between start and end
+                if login_time > time_start and login_time < time_end:
+
+                    # To add a match, use self.add_match
+                    self.add_match(document)
+
+    # The results of get_match_str will appear in the alert text
+    def get_match_str(self, match):
+        return "%s logged in between %s and %s" % (
+            match["username"],
+            self.rules["time_start"],
+            self.rules["time_end"],
+        )
+
+    # garbage_collect is called indicating that ElastAlert has already been run up to timestamp
+    # It is useful for knowing that there were no query results from Elasticsearch because
+    # add_data will not be called with an empty list
+    def garbage_collect(self, timestamp):
+        pass
---
+++ elastalert/custom_alerter/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="punch_plugins",
+    version="1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    author="punchplatform",
+    author_email="contact@punchplatform.com",
+    description="boilerplate for elastaler custom alerter, rule type, etc...",
+    python_requires='>=3.6',
+    install_requires=[
+        "pex==2.1.6",  # this is important do not remove
+        "requests==2.24.0",  # this is important do not remove
+    ]
+)
