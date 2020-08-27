96baa7180c7ee61f2f8a75d5c78588c404b6901b
Jonathan YUE CHUN <jonathan.yue-chun@mythalesgroup.io>
2020-08-27 10:50:35 +0200

    renamed param

--- spark/custom_punchlines_nodes/python/custom_node_python/nodes/my_use_case/my_node/complex_algorithm_post_execution.py
+++ spark/custom_punchlines_nodes/python/custom_node_python/nodes/my_use_case/my_node/complex_algorithm_post_execution.py
@@ -4,16 +4,16 @@
 from pyspark.sql.session import SparkSession
 
 
-class ComplexAlgorithmnInitializer(object):
-    """You can initialize sparksession object with additional values
+class ComplexAlgorithmnPostExecution(object):
+    """You can run some code after your punchline execution has finished
     """
 
     __spark_session: SparkSession
 
     def __init__(self) -> None:
         self.__spark_session = SparkSession.builder.getOrCreate()
-        self.configure()
+        self.post()
 
-    def configure(self) -> None:
+    def post(self) -> None:
         # do something with your spark session
-        print("configuration example before punchline execution")
+        print("configuration example post punchline execution")
