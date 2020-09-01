246fe61f109000717869e9ae0b04fedaf13134ab
pierre.dosne <pierre.dosne@thalesgroup.com>
2020-09-01 12:34:59 +0200

    Make rule runnable with standalone

--- elastalert/custom_modules/punch_plugins/enhancements/my_use_case/complex_enhancement.py
+++ elastalert/custom_modules/punch_plugins/enhancements/my_use_case/complex_enhancement.py
@@ -10,6 +10,6 @@ class ComplexEnhancement(BaseEnhancement):
     # The match is passed to the process function where it can be modified in any way
     # ElastAlert will do this for each enhancement linked to a rule
     def process(self, match):
-        if 'domain' in match:
-            url = "http://who.is/whois/%s" % (match['domain'])
-            match['domain_whois_link'] = url
+        if "standalone" in match["platform"]["id"] :
+            description = "This is a standalone platform"
+            match['description'] = description
--- elastalert/custom_modules/punch_plugins/rules/my_use_case/complex_rule.py
+++ elastalert/custom_modules/punch_plugins/rules/my_use_case/complex_rule.py
@@ -17,34 +17,33 @@ class ComplexRule(RuleType):
     # You can ensure that the rule config file specifies all
     # of the options. Otherwise, ElastAlert will throw an exception
     # when trying to load the rule.
-    required_options = set(["time_start", "time_end", "usernames"])
+    required_options = set(["time_start", "time_end", "kafka_topic_name"])
 
     # add_data will be called each time Elasticsearch is queried.
     # data is a list of documents from Elasticsearch, sorted by timestamp,
     # including all the fields that the config specifies with "include"
     def add_data(self, data):
         for document in data:
-
-            # To access config options, use self.rules
-            if document["username"] in self.rules["usernames"]:
-
-                # Convert the timestamp to a time object
-                login_time = document["@timestamp"].time()
-
-                # Convert time_start and time_end to time objects
-                time_start = dateutil.parser.parse(self.rules["time_start"]).time()
-                time_end = dateutil.parser.parse(self.rules["time_end"]).time()
-
-                # If the time falls between start and end
-                if login_time > time_start and login_time < time_end:
-
-                    # To add a match, use self.add_match
-                    self.add_match(document)
+            
+            try: 
+                # To access config options, use self.rules
+                if document["kafka"]["topic"] in self.rules["kafka_topic_name"]:
+                    # Convert the timestamp to a time object
+                    login_time = document["@timestamp"].time()
+                    # Convert time_start and time_end to time objects
+                    time_start = dateutil.parser.parse(self.rules["time_start"]).time()
+                    time_end = dateutil.parser.parse(self.rules["time_end"]).time()
+                    # If the time falls between start and end
+                    if login_time > time_start and login_time < time_end:
+                        # To add a match, use self.add_match
+                        self.add_match(document)
+            except:
+                print("kafka topic is not a valid key")
 
     # The results of get_match_str will appear in the alert text
     def get_match_str(self, match):
         return "%s logged in between %s and %s" % (
-            match["username"],
+            match["kafka"]["topic"],
             self.rules["time_start"],
             self.rules["time_end"],
         )
